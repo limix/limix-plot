1bbcc191afe6a487d43ed9237cce5189ce410a64
Danilo Horta <danilo.horta@gmail.com>
2018-04-09 15:33:39 +0100

    datasert comment

--- limix_plot/dataset.py
+++ limix_plot/dataset.py
@@ -13,6 +13,17 @@ else:
 
 
 def load_dataset(name):
+    r"""Example datasets.
+
+    Parameters
+    ----------
+    name : str
+        Possible dataset names are ``"kinship"``, ``"dali"``, and ``"gwas"``.
+
+    Returns
+    -------
+    Numpy array, BytesIO, DataFrame : Selected dataset.
+    """
     if name == 'kinship':
         c = urlopen("http://rest.s3for.me/limix/1000G_kinship.npy").read()
         f = io.BytesIO(c)
@@ -27,20 +38,3 @@ def load_dataset(name):
         return read_pickle(io.BytesIO(o))
 
     raise ValueError("Unknown dataset {}.".format(name))
-
-
-# import sys
-# import bz2
-# import io
-# import limix_plot as lp
-# from matplotlib import pyplot as plt
-#
-# PY2 = sys.version_info < (3, )
-#
-# if PY2:
-#     from urllib import urlopen
-# else:
-#     from urllib.request import urlopen
-#
-# lp.image(f)
-# plt.show()
